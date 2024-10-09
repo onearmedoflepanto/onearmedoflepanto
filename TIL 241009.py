4c7acf31b0fa46d2f53fd7559bef0d5e21d5d71a
onearmedoflepanto <high.kick.jake@gmail.com>
2024-10-09 23:41:27 +0900

    Create TIL 241009.py

---
+++ TIL 241009.py
@@ -0,0 +1,46 @@
+'''
+이 코드들은 코드업(codeup.kr)의 파이썬 기초 100제 풀이 코드입니다.
+정답은 참고하지 않았고(문제를 풀어야 정답을 볼 수 있음) 전부 자력으로 풀었어용
+'''
+
+#문제 1~10(출력)
+
+print("Hello")
+print("Hello World")
+print("Hello")
+print("World")
+print("'Hello'")
+print('"Hello World"')
+print("\"!@#$%^&*()'")
+print("\"C:\\Download\\'hello'.py\"")
+print("print(\"Hello\\nWorld\")")
+
+#문제 10~20(입력)
+
+a=input()
+print(a, a, a)
+
+a,b=input().split(':')
+print(a, b, sep=':')
+
+y,m,d=input().split('.')
+print(d, m, y, sep='-')
+
+rescode1,rescode2=input().split('-')
+print(rescode1+rescode2)
+
+s=input()
+print(s[0])
+print(s[1])
+print(s[2])
+print(s[3])
+print(s[4])
+
+a,b=input().split(':')
+print(a, b, sep=':')
+
+y,m,d=input().split('.')
+print(d, m, y, sep='-')
+
+rescode1,rescode2=input().split('-')
+print(rescode1+rescode2)
