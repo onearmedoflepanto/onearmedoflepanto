f2e61ab2cf60f4803a15698395ace12e6395e160
jo_dndud <doriconi@gmail.com>
2025-02-05 15:43:56 +0900

    정보갱신

--- 교본.py
+++ 교본.py
@@ -1,9 +1,10 @@
-
-counter=1
-arr=[[0]*3 for _ in range(3)]
+counter = 1
+arr = [[0] * 3 for _ in range(3)]
 
 for i in range(3):
     for j in range(3):
-        arr[i][j]=counter
-        counter+=1
+        arr[i][j] = counter
+        counter += 1
         print(arr)
+
+# GIT 갱신용 주석
