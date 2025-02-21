493ec3d8cec529eba7dc469b9af555873d823c91
이상욱 <lsw0152@naver.com>
2025-02-21 11:42:46 +0900

    새자리 푸시설정

--- testbed.py
+++ testbed.py
@@ -1,28 +1,30 @@
 t_input = int(input())
 
-for t in range(1,t_input+1):
+for t in range(1, t_input+1):
     n = int(input())
-    a=[]
-    b=[]
+    a = []
+    b = []
     for _ in range(n):
-        a_val, b_val = map(int,input().split())
+        a_val, b_val = map(int, input().split())
         a.append(a_val)
         b.append(b_val)
 
     p = int(input())
 
-    c=[]
+    c = []
 
     for _ in range(p):
         c_val = int(input())
         c.append(c_val)
 
-    dat =[0] * 5001
+    dat = [0] * 5001
 
     for i in range(n):
         for j in range(a[i], b[i]+1):
             dat[j] += 1
 
-    print(f"#{t}",end=' ')
+    print(f"#{t}", end=' ')
     for num in c:
-        print(dat[num],end=' ')
+        print(dat[num], end=' ')
+
+# new test code for github push
